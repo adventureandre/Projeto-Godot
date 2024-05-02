a6ada74e2ad492cc282be6dff326f8c45f141ae2
Andre Luiz <adventureandre@hotmail.com>
2024-05-01 23:44:40 -0300

    Configurando Thema para a Base

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ThemeProvider } from 'styled-components'
 import { AppRouter } from './Routers'
 import { GlobalStyles } from './styles/global'
-import { darkTheme } from './styles/theme'
+import { darkTheme, lightTheme } from './styles/theme'
+import { useState } from 'react'
 
 export function App() {
+  const [isLightTheme, setIsLightTheme] = useState<boolean>(false)
+
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={isLightTheme ? lightTheme : darkTheme}>
       <AppRouter />
       <GlobalStyles />
     </ThemeProvider>
